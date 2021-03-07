d9dc1eba4312fb165760a7bed0accf0d7a40da49
hassansribet <sribethassan@gmail.com>
2021-03-08 00:33:12 +0100

    add todo-item to todos page

--- src/app/_pages/todos/todos.component.ts
+++ src/app/_pages/todos/todos.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder } from '@angular/forms';
+
+import { Item } from '../../_shared/models/item.model';
+
+
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  todos: Item[] = [
+    {
+      id: 1,
+      label: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum enim error excepturi libero nam pariatur quibusdam temporibus! Ab accusamus adipisci deleniti eos excepturi harum necessitatibus nemo officia optio quam, rem.',
+      checked: false
+    },
+    {
+      id: 2,
+      label: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab commodi consequuntur dignissimos dolorum esse fuga iure modi molestiae non officiis possimus quam reprehenderit sapiente similique sint tempore ullam velit, voluptatem.',
+      checked: false
+    }
+  ];
+  addNewToDo = this.formBuilder.group({
+    label: ''
+  });
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    console.log(this.addNewToDo);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import {AppRoutingModule} from './app-routing.module';
 import { TodosComponent } from './_pages/todos/todos.component';
+import { TodoItemComponent } from './_components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
